c224a6a7c1a738d3d6bec9d8d593ec7d14c14a57
Pri076 <priyanka.gondaliya1102@gmail.com>
2022-01-10 19:19:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/finalquiz/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.quizzer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import android.widget.TextView
+import com.example.finalquiz.R
+
+class MainActivity : AppCompatActivity() {
+
+    var questionNo = 0
+    var questions = listOf("What are the two official languages for Android development? \n\n A) Kotlin and Java \n\n B) Java and Python \n\n C) Kotlin and Python", "How do you define a function in Kotlin? \n\n A) void \n\n B) var \n\n C) function", "What is a variable used for? \n\n A) To contain data \n\n B) To insert a random value \n\n C) Don't know", "What does SDK stand for in Android SDK? \n\n A) Software Development Kit \n\n B) Software Development Kotlin \n\n C) Something Don't Know")
+    var rightAnswers = listOf(1, 3, 2, 2)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button> (R.id.button)
+        val button2 = findViewById<Button> (R.id.button2)
+        val button3 = findViewById<Button> (R.id.button3)
+
+        button.setOnClickListener {
+            showToast(1)
+        }
+
+        button2.setOnClickListener {
+            showToast(2)
+        }
+
+        button3.setOnClickListener {
+            showToast(3)
+        }
+
+    }
+    fun showToast(answer: Int) {
+        if (answer==rightAnswers.get(questionNo)) {
+            Toast.makeText(applicationContext, "CORRECT!", Toast.LENGTH_SHORT).show()
+            updateQuestion()
+        } else {
+            Toast.makeText(applicationContext, "WRONG!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun updateQuestion() {
+        val textView = findViewById<TextView>(R.id.textView)
+        questionNo = questionNo + 1
+        textView.setText(questions.get(questionNo))
+    }
+
+}
